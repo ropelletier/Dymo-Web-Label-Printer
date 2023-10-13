601740ecdc9748db016f0e28445dd805f5082165
ropelletier <88781432+ropelletier@users.noreply.github.com>
2023-10-13 12:12:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+#https://github.com/dymosoftware/Drivers
+#https://www.reddit.com/r/dymo/comments/14zv98i/how_to_use_dymo_5xl_on_linux_ubuntu_based_distros/
+apt update
+git clone https://github.com/dymosoftware/Drivers
+apt-get install libboost-all-dev build-essential autotools-dev libcups2-dev libcupsimage2-dev cups -y
 
+
+bash ./configure
+autoreconf -ivf
+make
